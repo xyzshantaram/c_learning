fb3dc1d34be1ae485b4f47f38eaa11d3bc1812d9
Siddharth S Singh <simsid66@gmail.com>
2020-11-05 10:36:43 +0530

    kilo chapter 2

--- kilo/kilo.c
+++ kilo/kilo.c
@@ -1,3 +1,68 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
+
+/*** defines ***/
+#define CTRL_KEY(k) ((k)&0x1f)
+/*** data ***/
+/*** terminal ***/
+/*** init ***/
+
+struct termios orig_termios;
+
+void die(const char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+void disable_raw_mode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+void enable_raw_mode() {
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+        die("tcgetattr");
+    atexit(disable_raw_mode);
+    struct termios raw = orig_termios;
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1)
+        die("tcsetattr");
+}
+
+char editor_read_key() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+
+void editor_process_keypress() {
+    char c = editorReadKey();
+    switch (c) {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+    }
+}
+
 int main() {
+    enable_raw_mode();
+    while (1) {
+        editorProcessKeypress();
+    }
+    disable_raw_mode();
     return 0;
 }
